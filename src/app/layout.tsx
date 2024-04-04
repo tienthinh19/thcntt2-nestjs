1a30755f9eed68394eed9066ffc74941538d4d0e
tienthinh19 <thinhbui1907@gmail.com>
2024-04-04 18:01:29 +0700

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,38 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="calculator">
-    <div className="input" id="input"></div>
-    <div className="buttons">
-      <div className="operators">
-        <div>+</div>
-        <div>-</div>
-        <div>&times;</div>
-        <div>&divide;</div>
-      </div>
-      <div className="leftPanel">
-        <div className="numbers">
-          <div>7</div>
-          <div>8</div>
-          <div>9</div>
-        </div>
-        <div className="numbers">
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
-        </div>
-        <div className="numbers">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-        </div>
-        <div className="numbers">
-          <div>0</div>
-          <div>.</div>
-          <div id="clear">C</div>
-        </div>
-      </div>
-      <div className="equal" id="result">=</div>
+    <div className="login-page">
+    <div className="form">
+      <form className="login-form">
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+        <button>login</button>
+        
+      </form>
     </div>
   </div>
   );
